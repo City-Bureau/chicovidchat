6f56151b2a874623a88a84a50c2a856463bdcc26
pjsier <pjsier@gmail.com>
2020-04-08 15:29:19 -0500

    refactor: remove scrapers

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.14
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.4.1
-	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/aws/aws-lambda-go v1.16.0
 	github.com/aws/aws-sdk-go v1.30.3
 	github.com/jinzhu/gorm v1.9.12
--- pkg/directory/resource.go
+++ pkg/directory/resource.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sort"
 	"strings"
 	"time"
 
@@ -98,51 +97,6 @@ func translateSlice(items []string, localizer *i18n.Localizer) string {
 	return strings.Join(translatedItems, ", ")
 }
 
-func (r *Resource) updateChangedFields(resource Resource) bool {
-	changed := false
-	if r.Name != resource.Name && resource.Name != "" {
-		r.Name = resource.Name
-		changed = true
-	}
-	if r.Link != resource.Link && resource.Link != "" {
-		r.Link = resource.Link
-		changed = true
-	}
-	if r.Description != resource.Description && resource.Description != "" {
-		r.Description = resource.Description
-		changed = true
-	}
-	if r.Hours != resource.Hours && resource.Hours != "" {
-		r.Hours = resource.Hours
-		changed = true
-	}
-	if r.Address != resource.Address && resource.Address != "" {
-		r.Address = resource.Address
-		changed = true
-	}
-	if r.ZIP != resource.ZIP && resource.ZIP != "" {
-		r.ZIP = resource.ZIP
-		changed = true
-	}
-	if slicesEqual(r.Category, resource.Category) && len(resource.Category) > 0 {
-		r.Category = resource.Category
-		changed = true
-	}
-	if slicesEqual(r.Who, resource.Who) && len(resource.Who) > 0 {
-		r.Who = resource.Who
-		changed = true
-	}
-	if r.Level != resource.Level && resource.Level != "" {
-		r.Level = resource.Level
-		changed = true
-	}
-	if r.Type != resource.Type && resource.Type != "" {
-		r.Type = resource.Type
-		changed = true
-	}
-	return changed
-}
-
 type airtableRecord struct {
 	ID     string   `json:"id"`
 	Fields Resource `json:"fields"`
@@ -212,41 +166,6 @@ func LoadAirtableResources(base, table, key string) ([]Resource, error) {
 	return resources, nil
 }
 
-// SyncAirtableResources takes a slice of resources and updates or creates them based on ExternalID
-func SyncAirtableResources(resources []Resource, base, table, key string) error {
-	recordIDMap := map[string]airtableRecord{}
-	records, err := loadAirtableRecords(base, table, key)
-	if err != nil {
-		return err
-	}
-
-	for _, r := range records {
-		if r.Fields.ExternalID != "" {
-			recordIDMap[r.Fields.ExternalID] = r
-		}
-	}
-
-	for _, r := range resources {
-		// TODO: Make sure fields aren't dropped (modify?)
-		if updateRec, ok := recordIDMap[r.ExternalID]; ok {
-			changed := updateRec.Fields.updateChangedFields(r)
-			if changed {
-				updateErr := createOrUpdateAirtableResource(updateRec.Fields, &updateRec.ID, base, table, key)
-				if updateErr != nil {
-					return updateErr
-				}
-			}
-		} else {
-			createErr := createOrUpdateAirtableResource(r, nil, base, table, key)
-			if createErr != nil {
-				return createErr
-			}
-		}
-	}
-	log.Println(len(records))
-	return nil
-}
-
 // LoadResources pulls the latest resource items from S3
 func LoadResources() ([]Resource, error) {
 	var resources []Resource
@@ -273,33 +192,4 @@ func LoadResources() ([]Resource, error) {
 	}
 
 	return resources, nil
-}
-
-func createOrUpdateAirtableResource(resource Resource, id *string, base, table, key string) error {
-	method := "POST"
-	reqURL := fmt.Sprintf("https://api.airtable.com/v0/%s/%s", base, table)
-	if id != nil {
-		method = "PATCH"
-		reqURL = fmt.Sprintf("%s/%s", reqURL, *id)
-	}
-	// TODO: Include body/fields
-	client := &http.Client{}
-	req, _ := http.NewRequest(method, reqURL, nil)
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", key))
-	_, err := client.Do(req)
-	return err
-}
-
-func slicesEqual(a, b []string) bool {
-	sort.Strings(a)
-	sort.Strings(b)
-	if len(a) != len(b) {
-		return false
-	}
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		}
-	}
-	return true
 }
