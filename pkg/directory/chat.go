953afe9e371bfaa9e46f1874f7a0bdb8bd73a63a
pjsier <pjsier@gmail.com>
2020-04-22 09:24:47 -0500

    feat: add zh locale

--- pkg/directory/chat.go
+++ pkg/directory/chat.go
@@ -75,7 +75,7 @@ func UpdateDirectoryChatConversation(directoryChat *DirectoryChat, conversation 
 }
 
 func languageOptions() []string {
-	return []string{"en", "es", "pl", "tl", "vi"}
+	return []string{"en", "es", "zh", "pl", "tl", "vi"}
 }
 
 // Values should be IDs for i18n messages
